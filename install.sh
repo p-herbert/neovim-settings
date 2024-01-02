8abeed13ce476dff2b8c069fead5aadc9c27ea94
p-herbert <11701411+p-herbert@users.noreply.github.com>
2024-01-02 13:48:46 -0800

    (fix) Correct typo

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 set -evx
 
-PYTHON_2_VERION=2.7.18
+PYTHON_2_VERSION=2.7.18
 PYTHON_3_VERSION=3.9.18
 
 SCRIPT_DIR=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
@@ -27,10 +27,10 @@ ln -sf "${SCRIPT_DIR}/pack" "$HOME/.config/nvim"
 ln -sf "${SCRIPT_DIR}/coc-settings.json" "$HOME/.config/nvim/coc-settings.json"
 
 # Set python environments
-: pyenv install "${PYTHON_2_VERION}"
+: pyenv install "${PYTHON_2_VERSION}"
 : pyenv install "${PYTHON_3_VERSION}"
 
-pyenv virtualenv "${PYTHON_2_VERION}" neovim2
+pyenv virtualenv "${PYTHON_2_VERSION}" neovim2
 pyenv virtualenv "${PYTHON_3_VERSION}" neovim3
 
 pyenv activate neovim2
