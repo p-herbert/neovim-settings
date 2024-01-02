926df7897b04c936cb434e4f346cb7dcee0b5186
p-herbert <11701411+p-herbert@users.noreply.github.com>
2024-01-02 13:49:30 -0800

    (refactor) Create function python_setup

--- install.sh
+++ install.sh
@@ -13,6 +13,21 @@ cleanup() {
   [[ -n "$VIRTUAL_ENV" ]] && pyenv deactivate
 }
 
+python_setup() {
+  local PYTHON_VERSION=$1
+  local VIRTUAL_ENVIRONMENT_NAME=$2
+
+  : CONFIGURE_OPTS="--with-openssl=$(brew --prefix openssl)" pyenv install "${PYTHON_VERSION}"
+
+  : pyenv virtualenv "${PYTHON_VERSION}" "${VIRTUAL_ENVIRONMENT_NAME}"
+
+  pyenv activate "${VIRTUAL_ENVIRONMENT_NAME}"
+
+  pip install neovim pynvim
+
+  pyenv deactivate
+}
+
 # Create required directory
 mkdir -p "$SCRIPT_DIR/pack/plugins/start" "$HOME/.config/nvim"
 
@@ -29,16 +44,5 @@ ln -sf "${SCRIPT_DIR}/coc-settings.json" "$HOME/.config/nvim/coc-settings.json"
 eval "$(pyenv init -)"
 
 # Set python environments
-: pyenv install "${PYTHON_2_VERSION}"
-: pyenv install "${PYTHON_3_VERSION}"
-
-pyenv virtualenv "${PYTHON_2_VERSION}" neovim2
-pyenv virtualenv "${PYTHON_3_VERSION}" neovim3
-
-pyenv activate neovim2
-pip install neovim pynvim
-pyenv deactivate
-
-pyenv activate neovim3
-pip install neovim pynvim
-pyenv deactivate
+python_setup "${PYTHON_2_VERSION}" neovim2
+python_setup "${PYTHON_3_VERSION}" neovim3
